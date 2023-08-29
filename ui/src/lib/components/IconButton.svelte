b639bcb05f4ebc9b150445b46596be20f215a2ab
orw22 <oliver@thewortleys.plus.com>
2023-08-29 17:57:56 +0100

    Icon components

---
+++ ui/src/lib/components/IconButton.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let disabled: boolean;
+  export let onClick: () => void;
+  export let type: "button" | "submit" | "reset" = "button";
+</script>
+
+<button class="icon-button" {disabled} on:click={onClick} {type}>
+  <slot />
+</button>
+
+<style>
+  /* TODO */
+</style>
---
+++ ui/src/lib/components/icons/Play.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export let size: number;
+  export let colour: string;
+</script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 20 20"
+  width={size}
+  height={size}
+  fill={colour}
+>
+  <path
+    d="M6.3 2.841A1.5 1.5 0 004 4.11V15.89a1.5 1.5 0 002.3 1.269l9.344-5.89a1.5 1.5 0 000-2.538L6.3 2.84z"
+  />
+</svg>

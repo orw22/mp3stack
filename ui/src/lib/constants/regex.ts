30e369c16a7bdd40bb6f37c41591a2c88d14c952
orw22 <oliver@thewortleys.plus.com>
2023-09-02 23:26:35 +0100

    Regex docs

--- ui/src/lib/constants/regex.ts
+++ ui/src/lib/constants/regex.ts
@@ -1,5 +1,31 @@
+/**
+ * Regex for matching alphabetic characters and spaces.
+ * @type {RegExp}
+ */
 export const ALPHA_PATTERN = /^[a-zA-Z ]{0,64}$/;
+
+/**
+ * Regex for matching alphanumeric characters and spaces.
+ * @type {RegExp}
+ */
 export const ALPHANUMERIC_PATTERN = /^[a-zA-Z0-9 ]{0,64}$/;
+
+/**
+ * Regex for matching valid email addresses.
+ * @type {RegExp}
+ */
 export const EMAIL_PATTERN = /^[a-zA-Z0-9._\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$/;
+
+/**
+ * Regular expression pattern for strong passwords with following requirements:
+ * - At least one symbol (!@#$%^&*()_+{}[]:;<>,.?~-)
+ * - At least one digit (0-9)
+ * - At least one lowercase letter (a-z)
+ * - At least one uppercase letter (A-Z)
+ * - Length between 10 and 128 characters.
+ *
+ * @see https://stackoverflow.com/questions/76285652/valid-with-the-regexp-u-flag-but-not-with-the-v-flag for reason behind escape characters.
+ * @type {RegExp}
+ */
 export const PASSWORD_PATTERN =
   /^(?=.*[!@#$%^&*\(\)_+\{\}\[\]:;<>,.?~\-]).*(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{10,128}$/;

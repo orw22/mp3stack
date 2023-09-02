ffbc7a46372d6409f42d10b686eb0e3ae9b9801d
orw22 <oliver@thewortleys.plus.com>
2023-09-02 01:43:17 +0100

    Adjust time input window

--- ui/src/lib/components/Playbar/AudioControls.svelte
+++ ui/src/lib/components/Playbar/AudioControls.svelte
@@ -17,7 +17,7 @@
   import Icon from "../Icon.svelte";
   import IconButton from "../IconButton.svelte";
 
-  const TIME_INPUT_WINDOW = 25;
+  const TIME_INPUT_WINDOW = 50;
   const TIME_UPDATE_WINDOW = 100;
 
   let audioEl: HTMLAudioElement;
@@ -49,7 +49,7 @@
       wasPaused[1] = true;
     }
 
-    if (prevTimeInputEvent > event.timeStamp - TIME_INPUT_WINDOW) {
+    if (event.timeStamp < prevTimeInputEvent + TIME_INPUT_WINDOW) {
       // if seeking/dragging slider
       audioEl.pause();
     } else {
